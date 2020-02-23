7efe9d3fb1fbdcca6f0e3e2058654d69167293d9
ARAVIND <arvindputtu@gmail.com>
2020-02-23 12:01:08 +0530

    Updates

---
+++ precache-manifest.1ade4acf1d650fdc964edc3fd1cdbdfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6734012736915ebfbea6cde4d079687",
+    "url": "/deploy-react-IMDB/index.html"
+  },
+  {
+    "revision": "c1548aafd839e20fd888",
+    "url": "/deploy-react-IMDB/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "0314840cf1f97c7ada13",
+    "url": "/deploy-react-IMDB/static/js/2.c1249fae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/deploy-react-IMDB/static/js/2.c1249fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1548aafd839e20fd888",
+    "url": "/deploy-react-IMDB/static/js/main.69ebf654.chunk.js"
+  },
+  {
+    "revision": "9c92aa2ea6cd2a409bd1",
+    "url": "/deploy-react-IMDB/static/js/runtime-main.d74d1ac7.js"
+  }
+]);
